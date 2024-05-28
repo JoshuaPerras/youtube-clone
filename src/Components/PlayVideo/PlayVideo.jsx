35101945c888cec1896c0ee6c19c9dcf7fc16572
Joshua Perras <JoshPerras11@gmail.com>
2024-05-27 22:38:15 -0400

    Channel data for logo and sub count

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -7,14 +7,16 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
-import { API_KEY } from '../../data'
+import { API_KEY, value_converter } from '../../data'
 import { useEffect, useState } from 'react'
+import moment from 'moment'
 
 
 
 const PlayVideo = ({videoId}) => {
 
     const [apiData, setApiData] = useState(null);
+    const[channelData, setChannelData] = useState(null);
 
     const fetchVideoData = async () =>{
         // fetching videos Data
@@ -22,9 +24,19 @@ const PlayVideo = ({videoId}) => {
         await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]));
     }
 
+    const fetchOtherData = async () =>{
+        // Fetching Channel data
+        const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+        await fetch(channelData_url).then(res=>res.json()).then(data => setChannelData(data.items[0]));
+    }
+
     useEffect(()=>{
         fetchVideoData();
     },[])
+
+    useEffect(()=>{
+        fetchOtherData();
+    },[apiData])   
 
   return (
     <div className='play-video'>
@@ -32,28 +44,27 @@ const PlayVideo = ({videoId}) => {
         <iframe src= {`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
         <div className="play-video-info">
-            <p>1525 Views &bull; 2 days ago</p>
+            <p> {apiData?value_converter(apiData.statistics.viewCount):"Views"} &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
             <div>
-                <span><img src={like} alt="" /> 125</span>
-                <span><img src={dislike} alt="" /> 2</span>
+                <span><img src={like} alt="" /> {apiData?value_converter(apiData.statistics.likeCount):404}</span>
+                <span><img src={dislike} alt="" /> </span>
                 <span><img src={share} alt="" /> Share</span>
                 <span><img src={save} alt="" /> Save</span>
             </div>
         </div>
         <hr />
         <div className="publisher">
-            <img src={jack} alt="" />
+            <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
             <div>
-                <p>GreatStack</p>
-                <span>1M Subscribers</span>
+                <p>{apiData?apiData.snippet.channelTitle:"Publisher"}</p>
+                <span> {channelData?value_converter(channelData.statistics.subscriberCount):404} Subscribers</span>
             </div>
             <button>Subscribe</button>
         </div>
         <div className="vid-description">
-            <p>Channel that makes learning easy</p>
-            <p>Subscribe GreatStack to Watch More Tutorials on web development</p>
+            <p>{apiData?apiData.snippet.description.slice(0,250):"Description here"}</p>
             <hr />
-            <h4>130 Comments</h4>
+            <h4>{apiData?value_converter(apiData.statistics.commentCount):"404"} Comments</h4>
             <div className="comment">
                 <img src={user_profile} alt="" />
                 <div>
